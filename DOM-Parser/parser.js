c216a157b8da385cf474a0db189eae1547aaf8c6
Sarah <sarahchristen74@gmail.com>
2024-11-14 11:53:28 +0100

    DOM-Parser

---
+++ DOM-Parser/parser.js
@@ -0,0 +1,24 @@
+// Wichtig: die result Variable ausserhalb von parseNode() deklarieren, 
+// sonst wird sie bei rekursivem Aufruf überschrieben (d.h. neu deklariert)  
+var result = "";
+
+function parseNode(startNode) {
+    
+    for (var i = 0; i < startNode.childNodes.length; i++) {
+   
+        var nd =  startNode.childNodes[i];
+       
+        result += "<tr>"; 
+        // TODO: die Variable result weiter mit den den gewünschten Daten befüllen  
+        result += "<td>" + nd.nodeType + "</td>"
+        result += "<td>" + nd.nodeName + "</td>"
+        result += "<td>" + nd.nodeValue + "</td>"
+        result += "<td>" + nd.textContent + "</td>"
+        result += "</tr>"; 
+
+        // nächsten Node parsen durch rekursiven Aufruf dieser Funktion
+        parseNode(nd);
+    }
+
+    return "<table><tr><th>nodeType</th><th>nodeName</th><th>nodeValue</th><th>textContent</th></tr>" + result + "</table>";
+}
